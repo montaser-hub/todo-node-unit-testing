c8c14c5062a070e76e2f8f1071bfea30bd8ebd77
Salsabeel <58127260+sals1995@users.noreply.github.com>
2025-02-27 10:52:31 +0200

    lecture is done

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,18 +4,20 @@ const { APIError } = require('../utilities/errors')
 
 //authentication
 async function auth(req, res, next) {
-    if (!req.headers.authorization) {
-       next( new APIError(401, 'you have not access , please login first' ))
-    }
-
-    try{
+  
+  try{
+      if (!req.headers.authorization) {
+         next( new APIError(401, 'you have not access , please login first' ))
+      }
+      else{
         var decoded = await promisify(jwt.verify)(req.headers.authorization,process.env.SECRET)
         req.id=decoded.id
     
         next()
+      }
     }catch(err){
 
-      next( new APIError(401, 'you are not authenticated'))
+      next( err)
     }
  
 }
